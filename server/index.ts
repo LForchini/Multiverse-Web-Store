35cf61978f733cb9f46f62bbafcafbc06ca17f55
Leonardo Forchini <31443269+LForchini@users.noreply.github.com>
2022-01-28 11:00:53 +0000

    Integrated front and back-end (#28)

--- server/index.ts
+++ server/index.ts
@@ -5,7 +5,7 @@ import { router as CartRouter } from "./src/routes/cart-route";
 import { loadFromSeed, sequelize } from "./src/sequelize";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
