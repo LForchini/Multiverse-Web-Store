f24ae8a557611125cf41902b709666cb73618d04
LForchini <leonardo.forchini@gmail.com>
2022-01-28 15:38:26 +0000

    Added tests to models

--- server/tests/models/cart.test.ts
+++ server/tests/models/cart.test.ts
@@ -1,4 +1,5 @@
 import Cart from "../../src/models/cart.model";
+import CartRow from "../../src/models/row.model";
 import { sequelize } from "../../src/sequelize";
 
 describe("Cart", () => {
@@ -12,5 +13,16 @@ describe("Cart", () => {
     expect(cookie.length).toBe(128);
     expect(cart.cookie).toEqual(cookie);
     expect(cookie).not.toEqual(cart.generateCookie());
+  });
+
+  it("can be created and saved", async () => {
+    const cart = new Cart();
+    const cookie = cart.generateCookie();
+    await cart.save();
+    await cart.reload({ include: [CartRow] });
+
+    expect(cart.cookie).toBe(cookie);
+    expect(cart.id).not.toBe(undefined);
+    expect(cart.rows.length).toBe(0);
   });
 });
