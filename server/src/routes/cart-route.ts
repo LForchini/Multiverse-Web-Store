59bc373d19ba59b6d1f10545c9df1240b97292a3
LForchini <leonardo.forchini@gmail.com>
2022-01-28 13:56:25 +0000

    Added basic server-side validation

--- server/src/routes/cart-route.ts
+++ server/src/routes/cart-route.ts
@@ -1,5 +1,5 @@
-import e from "express";
 import express, { Request, Response } from "express";
+import { check, validationResult } from "express-validator";
 import Cart from "../models/cart.model";
 import CartRow from "../models/row.model";
 
@@ -26,24 +26,36 @@ router.get("/", async (req: Request, res: Response) => {
   res.cookie("cart", cart.cookie).send(cart);
 });
 
-router.post("/add", async (req: Request, res: Response) => {
-  const cart = await getCartByCookie(req.cookies.cart);
-  res.cookie("cart", cart.cookie);
+router.post(
+  "/add",
+  [check("productId").not().isEmpty(), check("quantity").not().isEmpty()],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-  if (req.body.productId && req.body.quantity) {
+    const cart = await getCartByCookie(req.cookies.cart);
+    res.cookie("cart", cart.cookie);
+
     const row = new CartRow({ cartId: cart.id, ...req.body });
     row.save();
     res.sendStatus(200);
-  } else {
-    res.sendStatus(400);
   }
-});
+);
 
-router.patch("/", async (req: Request, res: Response) => {
-  const cart = await getCartByCookie(req.cookies.cart);
-  res.cookie("cart", cart.cookie);
+router.patch(
+  "/",
+  [check("productId").not().isEmpty(), check("quantity").not().isEmpty()],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-  if (req.body.productId && req.body.quantity) {
+    const cart = await getCartByCookie(req.cookies.cart);
+    res.cookie("cart", cart.cookie);
+
     const row = await CartRow.findOne({
       where: { cartId: cart.id, productId: req.body.productId },
     });
@@ -57,10 +69,8 @@ router.patch("/", async (req: Request, res: Response) => {
       temp_row.save();
     }
     res.sendStatus(200);
-  } else {
-    res.sendStatus(400);
   }
-});
+);
 
 router.delete("/:productId", async (req: Request, res: Response) => {
   const cart = await getCartByCookie(req.cookies.cart);
